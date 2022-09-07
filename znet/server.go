95468b46ad5a72b3766b2f38ec89fee4b166ff93
Beat0n <hujizheuestc@163.com>
2022-09-07 17:39:45 +0800

    “”

--- znet/server.go
+++ znet/server.go
@@ -1,6 +1,11 @@
 package znet
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+	"time"
+	"zinx/ziface"
+)
 
 type Server struct {
 	//Server Name
@@ -18,11 +23,75 @@ func (s *Server) Start() {
 	//开启一个go去做服务端Linster业务
 	go func() {
 		//1 获取TCP地址
-		addr, err :=
-		//2 监听服务器地址
+		addr, err := net.ResolveTCPAddr(s.IPVersion, fmt.Sprintf("%s:%d", s.IP, s.Port))
+		if err != nil {
+			fmt.Println("resolve tcp addr err: ", err)
+			return
+		}
 
-		   //监听成功
+		//2 监听服务器地址
+		listenner, err := net.ListenTCP(s.IPVersion, addr)
+		if err != nil {
+			fmt.Println("listen", s.IPVersion, "err", err)
+			return
+		}
+		//监听成功
+		fmt.Println("start Zinx server ", s.Name, "success, now listenning...")
 
 		//3 启动server网络连接
+		for {
+			conn, err := listenner.AcceptTCP()
+			if err != nil {
+				fmt.Println("Accept err ", err)
+				continue
+			}
+
+			go func() {
+				for {
+					buf := make([]byte, 512)
+					count, err := conn.Read(buf)
+					if err != nil {
+						fmt.Println("recv buf err ", err)
+						continue
+					}
+					//回显
+					if _, err := conn.Write(buf[:count]); err != nil {
+						fmt.Println("write back buf err ", err)
+						continue
+					}
+				}
+			}()
+		}
+	}()
+}
+
+func (s *Server) Stop() {
+	fmt.Println("[STOP] Zinx server , name ", s.Name)
+
+	//TODO  Server.Stop() 将其他需要清理的连接信息或者其他信息 也要一并停止或者清理
+}
+
+func (s *Server) Serve() {
+	s.Start()
+
+	//TODO Server.Serve() 是否在启动服务的时候 还要处理其他的事情呢 可以在这里添加
+
+	//阻塞,否则主Go退出， listenner的go将会退出
+	for {
+		time.Sleep(10 * time.Second)
 	}
+}
+
+/*
+  创建一个服务器句柄
+*/
+func NewServer(name string) ziface.IServer {
+	s := &Server{
+		Name:      name,
+		IPVersion: "tcp4",
+		IP:        "0.0.0.0",
+		Port:      7777,
+	}
+
+	return s
 }
